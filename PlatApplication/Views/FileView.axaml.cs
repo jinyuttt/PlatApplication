d9de57af2bc2923a150d909878a08424a5c3916b
jinyuttt <jinyu20072787@sina.com>
2020-07-30 01:38:36 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

---
+++ PlatApplication/Views/FileView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PlatClient.Views
+{
+    public class FileView : UserControl
+    {
+        public FileView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
