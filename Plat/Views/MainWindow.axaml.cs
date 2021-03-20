3fa21b68084c0684d3cf0e86b3cf48321881d02d
jinyuttt <jinyu20072787@sina.com>
2021-03-20 18:43:11 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

--- Plat/Views/MainWindow.axaml.cs
+++ Plat/Views/MainWindow.axaml.cs
@@ -1,12 +1,9 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.Ribbon;
-using Avalonia.Markup.Xaml;
-
+﻿using Avalonia.Markup.Xaml;
+using AvaloniaUI.Ribbon;
 
 namespace PlatForm.Views
 {
-    public class MainWindow : Window
+    public class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
